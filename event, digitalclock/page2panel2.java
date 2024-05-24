0cc578164f78c5b66db706ee6708f037ff39553c
yujinsung03 <165638002+yujinsung03@users.noreply.github.com>
2024-05-25 04:29:59 +0900

    Add files via upload

---
+++ event, digitalclock/page2panel2.java
@@ -0,0 +1,31 @@
+package objectproject;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class page2panel2 extends JPanel {
+	ImageIcon img;
+	JButton b1 = new JButton("매장 설명");
+	JButton b2 = new JButton("약도");
+	
+	public page2panel2() {	
+		img = new ImageIcon("nike.png");
+		JLabel imgLabel = new JLabel(img);
+		imgLabel.setBounds(2, 2, 250, 250);
+		add(imgLabel);
+		
+		setLayout(null);
+		
+		b1.setBounds(30, 270, 100, 30);
+		b2.setBounds(150, 270, 70, 30);
+		
+		add(b1);
+		add(b2);
+		
+		setBounds(0, 0, img.getIconWidth(), img.getIconHeight() + 50);
+		setVisible(true);
+	}
+}
